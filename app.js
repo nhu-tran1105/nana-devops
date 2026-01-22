d464297c52a26fce7e2439666b039ff6a69c8553
NhuTran <147178315+nhu-tran1105@users.noreply.github.com>
2026-01-22 12:50:49 -0600

    Migrate app.js to use ES6 import syntax

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-const express = require("express");
+import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello Express: DEV');
-});
+  res.send('Hello Express: DEV')
+})
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
-});
+})
